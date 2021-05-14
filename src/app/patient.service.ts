bb804ec40c92d2ce5e0658bf509681faaf3c08a5
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2021-05-14 16:53:17 -0400

    Clean imports

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, from, Observable, of, Subject } from 'rxjs';
+import { from, Observable, of, Subject } from 'rxjs';
 import { FhirAuthService } from './fhir-auth.service';
 import { filter, switchMap } from 'rxjs/operators';
-import Client from 'fhirclient/lib/Client';
-import Patient from "fhirclient/lib/Client"
+import {fhirclient} from 'fhirclient/lib/types';
+import Patient = fhirclient.FHIR.Patient;
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +23,7 @@ export class PatientService {
         if (client !== null) {
           const pid = client.getPatientId();
           if (pid !== null) this.patientID.next(pid);
-          
+
           return from(client.request(`Patient/${client.patient.id}`));
         } else {
           return of(null);
