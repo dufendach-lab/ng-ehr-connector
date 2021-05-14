868694ccf6d203ff5497ff2006af152990abb0d5
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2021-05-14 17:14:12 -0400

    Update routes to go to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {PatientInfoComponent} from "./patient-info/patient-info.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
   {
     path: 'dashboard',
     component: DashboardComponent,
