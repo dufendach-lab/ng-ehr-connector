9d7bdca10122099343e663d4888a8a0fc4ae4eb1
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2021-05-14 14:46:12 -0400

    Add patientId observable into fhir auth service

--- src/app/fhir-auth.service.ts
+++ src/app/fhir-auth.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import {from, Observable} from "rxjs";
 import Client from "fhirclient/lib/Client";
-import {fhirclient} from "fhirclient/lib/types";
-import AuthorizeParams = fhirclient.AuthorizeParams;
 import {oauth2} from "fhirclient";
 import {epicConfig, smartHealthIt} from "./env/endpoints";
-import {shareReplay} from "rxjs/operators";
+import {map, shareReplay} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FhirAuthService {
   public readonly client: Observable<Client|null>;
+  public readonly patientId: Observable<string|null>;
 
   constructor() {
     this.client = from(oauth2.ready()).pipe(shareReplay(1));
+    this.patientId = this.client.pipe(map(client => client?.getPatientId() || null));
   }
 
   authorize = oauth2.authorize;
