c48705513b7e4c2beaad048fc3b9e99aea7480a7
Callahan <Dillon.Callahan@cchmc.org>
2021-05-18 17:02:07 -0400

    Attempting to fix for loop error

--- src/app/fhir-auth.service.ts
+++ src/app/fhir-auth.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import {from, Observable} from "rxjs";
 import Client from "fhirclient/lib/Client";
 import {oauth2} from "fhirclient";
-import {epicConfig, smartHealthIt} from "./env/endpoints";
+import {epicConfig, FhirEndpoint, smartHealthIt} from "./env/endpoints";
 import {map, shareReplay, switchMap} from "rxjs/operators";
+import * as endpoints from '../assets/EpicEndpoints.json'
+import { Console } from 'console';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +15,14 @@ export class FhirAuthService {
   public readonly patientId: Observable<string|null>;
   public readonly authorized: Observable<boolean|null>;
 
+  private fhirEndpoints: FhirEndpoint[];
+
   constructor() {
     this.client = from(oauth2.ready()).pipe(shareReplay(1));
     this.patientId = this.client.pipe(map(client => client?.getPatientId() || null));
-    this.authorized = this.client.pipe(map(client => client && client.getPatientId() !== null))
+    this.authorized = this.client.pipe(map(client => client && client.getPatientId() !== null));
+
+    this.fhirEndpoints = this.getEndpoints();
   }
 
   authorize = oauth2.authorize;
@@ -30,5 +36,21 @@ export class FhirAuthService {
      console.log("Working");
      this.authorize(epicConfig);
     }
+  }
+
+  getEndpoints():FhirEndpoint[]{
+    const res: FhirEndpoint[] = []
+    console.log(endpoints);
+
+
+
+    endpoints.forEach(element => {
+      let entry = {} as FhirEndpoint;
+      entry.FHIRPatientFacingURI = element.FHIRPatientFacingURI;
+      entry.OrganizationName = element.OrganizationName;
+      //entry.clientId = EPIC_CLIENT_ID;
+      res.push(entry)
+    });
+    return res;
   }
 }
