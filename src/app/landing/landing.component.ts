8a998fa5a2d2edcfdb69a350226c0c1b09e3a74d
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2021-05-23 19:29:34 -0400

    Update landing page buttons

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
+import {FhirAuthService} from "../fhir-auth.service";
 
 @Component({
   selector: 'app-landing',
@@ -8,14 +9,16 @@ import { LoginComponent } from '../login/login.component';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  isAuthorized = this.fhirAuth.authorized;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private fhirAuth: FhirAuthService) { }
 
   email= '';
 
   ngOnInit(): void {
 
   }
+
   loginModal(){
     const dialogRef = this.dialog.open(LoginComponent, {
       width: '400px',
