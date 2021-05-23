4c5ac05b91d597a8162bf5e25e9eafed7efeeb3c
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2021-05-23 19:43:38 -0400

    Remove unused imports

--- src/app/authorize/authorize.component.ts
+++ src/app/authorize/authorize.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {Observable, Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
 import {first, map, startWith} from 'rxjs/operators';
 import {FhirAuthService} from "../fhir-auth.service";
 import {Router} from "@angular/router";
