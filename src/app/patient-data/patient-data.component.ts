7451c9f626dcf3b61fb39d3e8a6265429231898b
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2021-05-23 23:29:39 -0400

    Add category search to patient-data

--- src/app/patient-data/patient-data.component.ts
+++ src/app/patient-data/patient-data.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
 
-import { fhirclient }from 'fhirclient/lib/types';
+import {fhirclient} from 'fhirclient/lib/types';
+import {ObservationService} from '../observation.service';
+import {FormControl} from '@angular/forms';
 import Bundle = fhirclient.FHIR.Bundle;
 import Observation = fhirclient.FHIR.Observation;
-import { ObservationService } from '../observation.service';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-patient-data',
@@ -15,21 +15,38 @@ import { FormControl } from '@angular/forms';
 export class PatientDataComponent implements OnInit {
 
   ptData = new FormControl("29463-7");
+  category = new FormControl("vital-signs");
 
   obsBundle: Subject<Bundle | Observation> = new Subject();
 
-  actualPtData: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie lacinia metus vitae hendrerit. Sed scelerisque rutrum rutrum. Quisque a condimentum tortor, eu lobortis lacus. Vivamus in maximus augue, non fermentum arcu. Donec vitae ex non libero imperdiet mollis ut ac quam. Etiam ornare et neque iaculis convallis. Vivamus nec mauris vulputate, vulputate nunc sit amet, tincidunt libero. Etiam in ultricies turpis, nec facilisis libero. Proin sed eleifend risus. In at diam posuere, faucibus risus vitae, sollicitudin justo. In blandit augue interdum quam facilisis vehicula.';
-  dataReceived: boolean = false;
-  constructor(private obsService: ObservationService) { }
+  // obsBundle: Subject<Bundle | Observation> = new Subject();
+
+  constructor(private obsService: ObservationService) {
+  }
 
   ngOnInit(): void {
   }
 
   search(): void {
-    this.dataReceived = true;
-    this.actualPtData = this.ptData.value;
+    const code = this.ptData.value;
+    if (code) {
+      console.log(`getting code ${code}`);
+      this.obsService.getObservation(code).then(b => {
+        console.log('value returned:');
+        console.log(b);
+        this.obsBundle.next(b);
+      });
 
-    this.obsService.getObservation(this.actualPtData).then(b => this.obsBundle.next(b));
+    }
+
+  }
+
+  searchByCategory(): void {
+    const category = this.category.value;
+    if (category) {
+      this.obsService.getObservationByCategory(category).then(b => this.obsBundle.next(b));
+
+    }
   }
 
 }
