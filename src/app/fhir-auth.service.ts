e5697cb9f894ef0060a23dde924776cbca5f8299
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2021-05-23 23:26:06 -0400

    Convert auth component to use endpoint list

--- src/app/fhir-auth.service.ts
+++ src/app/fhir-auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {from, Observable, Subject} from "rxjs";
 import Client from "fhirclient/lib/Client";
 import {oauth2} from "fhirclient";
 import {epicConfig, FhirEndpoint, smartHealthIt} from "./env/endpoints";
 import {map, shareReplay} from "rxjs/operators";
-import * as data from '../assets/EpicEndpoints.json'
+// import * as endpointData from '../assets/EpicEndpoints.json'
+import * as endpointData from '../assets/test-epic-endpoints.json'
 
 interface EpicEndpoint {
   OrganizationName: string;
@@ -15,13 +16,12 @@ interface EpicEndpoint {
   providedIn: 'root'
 })
 export class FhirAuthService {
-  public readonly client: Observable<Client|null>;
-  public readonly patientId: Observable<string|null>;
-  public readonly authorized: Observable<boolean|null>;
-
-  private _client: Subject<Client|null> = new Subject();
-
-  private fhirEndpoints: FhirEndpoint[];
+  readonly client: Observable<Client | null>;
+  readonly patientId: Observable<string | null>;
+  readonly authorized: Observable<boolean | null>;
+  readonly authorize = oauth2.authorize;
+  readonly fhirEndpoints: FhirEndpoint[];
+  private _client: Subject<Client | null> = new Subject();
 
   constructor() {
     this.client = this._client.pipe(shareReplay(1));
@@ -31,21 +31,18 @@ export class FhirAuthService {
 
     from(oauth2.ready()).subscribe(client => this._client.next(client));
   }
-
-  authorize = oauth2.authorize;
-
-  testAuth(val: string): void {
-    if(val == 'SmartHealthIT'){
-      this.authorize(smartHealthIt);
-   }
-    else if(val == 'epicHealthService') {
-     this.authorize(epicConfig);
-    }
-  }
+  //
+  // testAuth(val: string): void {
+  //   if (val == 'SmartHealthIT') {
+  //     this.authorize(smartHealthIt);
+  //   } else if (val == 'epicHealthService') {
+  //     this.authorize(epicConfig);
+  //   }
+  // }
 
   //Uses Epic's provided Epic Endpoint to create a list of all te endpoints, then returns as a list of all endpoints
-  getEndpoints():FhirEndpoint[]{
-    const endpoints: EpicEndpoint[] = (data as any).default;
+  private getEndpoints(): FhirEndpoint[] {
+    const endpoints: EpicEndpoint[] = (endpointData as any).default;
     const res: FhirEndpoint[] = []
     console.log(endpoints);
     endpoints.forEach(element => {
