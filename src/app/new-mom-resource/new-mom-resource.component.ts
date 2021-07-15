605360c065c5f2c64d72be3ee0488666698b1cce
France <Christian.France@cchmc.org>
2021-07-15 13:36:49 -0400

    Added videos to resources

--- src/app/new-mom-resource/new-mom-resource.component.ts
+++ src/app/new-mom-resource/new-mom-resource.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-new-mom-resource',
   templateUrl: './new-mom-resource.component.html',
   styleUrls: ['./new-mom-resource.component.scss']
 })
-export class NewMomResourceComponent implements OnInit {
-
+export class NewMomResourceComponent {
   selectedResource = "";
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  ngOnInit(): void {
-  }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   openPDF(pdfFile: string): void {
     const fileName = pdfFile + '.pdf';
     const fileDir = '../../assets/pdfs/' + fileName;
     window.open(fileDir);
   }
+
 
 }
