7d4960a1f44b8438c96a2e46d4494ba47348fd12
France <Christian.France@cchmc.org>
2021-07-22 13:20:16 -0400

    Created contact page - needs email functionality

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  cForm = this.fb.group({
+    name: [''],
+    email: [''],
+    subject: [''],
+    concerns: ['']
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
