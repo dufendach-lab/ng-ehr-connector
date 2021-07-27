921a374b047e6515b8b90fbb2cdc0478be41a39d
France <Christian.France@cchmc.org>
2021-07-27 11:39:29 -0400

    Implemented mailto functionality (Email format needs changed)

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+export interface email {
+  name: string,
+  email: string,
+  content: string
+}
+
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
@@ -8,16 +14,26 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class ContactFormComponent implements OnInit {
 
+  body: string = "This is the body of the email";
+
   cForm = this.fb.group({
-    name: [''],
-    email: [''],
-    subject: [''],
-    concerns: ['']
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    concerns: ['', Validators.required]
   })
+
+  emailInfo = {} as email;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  sendEmail() {
+    // Email will need changed
+    // Change email format
+    const mail: string = "mailto:Christian.France@cchmc.org?subject=App%20Name%20Issue&body=Name: " + this.emailInfo.name + "%0D%0AEmail: " + this.emailInfo.email + "%0D%0A%0D%0A Issue: %0D%0A" + this.emailInfo.content
+    location.href = mail;
   }
 
 }
