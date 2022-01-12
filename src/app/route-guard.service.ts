5ec4fdc362fd40ba6f473f1384855681cda88e3d
ChFrance <chfrance15@gmail.com>
2022-01-12 10:24:16 -0500

    Added Admin Portal button to nav, added compat to routeguard & data service

--- src/app/route-guard.service.ts
+++ src/app/route-guard.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
+// import { AngularFireAuth } from '@angular/fire/compat/auth';
+// import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+// import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +10,9 @@ import { switchMap } from 'rxjs/operators';
 export class RouteGuardService implements CanActivate{
 
   constructor(public router: Router,
-    private afa: AngularFireAuth,
-    private afs: AngularFirestore) {}
+    // private afa: AngularFireAuth,
+    // private afs: AngularFirestore
+  ) {}
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
     let role = "";
