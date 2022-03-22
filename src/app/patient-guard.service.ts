3aba6fa34448c8f5d19b2da48bbb9a7da09e7c41
Dufendach, Kevin <KevinDufendach@users.noreply.github.com>
2022-03-22 13:16:45 -0400

    Update route guards to return URL tree instead of using tap redirects

--- src/app/patient-guard.service.ts
+++ src/app/patient-guard.service.ts
@@ -1,29 +1,28 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
-import { AuthService } from "./auth.service";
-import { take, map, tap} from "rxjs/operators";
-import {Observable} from "rxjs";
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router} from '@angular/router';
+import {AuthService} from "./auth.service";
+import {first, map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
-export class PatientGuardService implements CanActivate{
+export class PatientGuardService implements CanActivate {
 
-  constructor(public router: Router, private auth: AuthService) {}
+  constructor(private router: Router, private auth: AuthService) { }
 
-  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+  canActivate(route: ActivatedRouteSnapshot) {
 
     return this.auth.testing$.pipe(
-      take(1),
-      map(user => !!(user && user.roles.includes('Patient'))),
-      tap(isPatient => {
-        if(!isPatient) {
-          console.error('Access denied - patients only')
-          this.router.navigate(['/staff/landing'])
+      first(),
+      map(user => {
+        if (user && user.roles.includes('Patient')) {
+          return true
         }
+
+        console.warn('Access denied - patients only')
+        return this.router.parseUrl('/staff/landing');
       })
     )
 
   }
-
 }
